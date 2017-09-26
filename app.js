c04894aed1cc66d5fda8a6953f0393a20f804dda
Matthew Fricke <fricke9523@msn.com>
2017-09-26 05:33:02 -0600

    Checkpoint 8 events: completed

--- app.js
+++ app.js
@@ -1,45 +1,41 @@
 function onReady() {
-	const addToDoForm = document.getElementById('addToDoForm');
-	const newToDoText = document.getElementById('newToDoText');
-	const toDoList = document.getElementById('toDoList');
-	const deleteButton = document.getElementById("deleteButton");
-	addToDoForm.addEventListener('submit', (event) => {
+	var toDos = [];
+	var addToDoForm = document.getElementById("addToDoForm");
+
+	function createNewToDo(){
+		var newToDoText = document.getElementById("newToDoText");
+		toDos.push({
+			title: newToDoText.value,
+			complete: false
+		});
+		newToDoText.value = "";
+
+		renderTheUI(toDos);
+	}
+
+	function renderTheUI(toDos){
+		var todoList = document.getElementById("toDoList");
+
+		toDoList.innerHTML = "";
+
+		toDos.forEach(function(toDo){
+			var newLi = document.createElement("li");
+			var checkbox = document.createElement("input");
+			checkbox.type = "checkbox";
+
+			newLi.innerHTML = toDo.title;
+
+			todoList.appendChild(newLi);
+			newLi.appendChild(checkbox);
+		});
+	}
+
+	addToDoForm.addEventListener("submit", function(event){
 		event.preventDefault();
-
-		// get the text
-		let title = newToDoText.value;
-
-		// create a new li
-		let newLi = document.createElement('li');
-
-		//create a new input for the checkbox
-		let checkbox = document.createElement('input');
-
-		//set the input's type to checkbox
-		checkbox.type = "checkbox";
-
-		//set the title
-		newLi.textContent = title;
-
-		//attach it to the ul
-		toDoList.appendChild(newLi);
-
-		//attach the checkbox to the li
-		newLi.appendChild(checkbox);
-
-		//empty the input
-		newToDoText.value = '';
+		createNewToDo();
 	});
-	
-	deleteButton.addEventListener("click", function() {
-		var todo = document.querySelectorAll("LI");
-		var input = document.querySelectorAll("INPUT");
-			for(i = 0; i < input.length; i++){
-				if(input[i].checked === true){
-						input[i].parentNode.remove();
-				}
-			}
-	});
+
+	renderTheUI(toDos);
 }
 window.onload = function () {
 	onReady() ;
